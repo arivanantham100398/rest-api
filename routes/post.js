9042817e58c7198a5e2b822ea05b094c35c224e6
arivanantham100398 <arivanantham100398@gmail.com>
2023-03-25 08:25:03 +0530

    updated validation

--- routes/post.js
+++ routes/post.js
@@ -7,7 +7,7 @@ router.post("/", verifyToken, async (req, res) => {
   const newPost = new Post(req.body);
   try {
     const savedPost = await newPost.save();
-    res.status(200).json(savedPost);
+    res.status(200).json({ success: true, savedPost });
   } catch (err) {
     res.status(500).json(err);
   }
